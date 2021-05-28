028c073cd72c677df9b21213d57a7e0d44837c7b
Tal Gluck <tal@superconductive.com>
2021-05-28 15:28:24 -0400

    moree stuff

--- great_expectations/checkpoint/actions.py
+++ great_expectations/checkpoint/actions.py
@@ -84,13 +84,11 @@ class NoOpAction(ValidationAction):
 
 
 class IFTTTNotificationAction(ValidationAction):
-    def __init__(
-        self,
-        data_context,
-        webhook,
-    ):
+    def __init__(self, data_context, webhook, action_if_pass=None, action_if_fail=None):
         super().__init__(data_context)
         self.webhook = webhook
+        self.action_if_pass = action_if_pass
+        self.action_if_fail = action_if_fail
         assert webhook, "No webhook found in action config."
 
     def _run(
@@ -100,17 +98,21 @@ class IFTTTNotificationAction(ValidationAction):
         data_asset=None,
         payload=None,
     ):
-        webhook = self.webhook
+        if validation_result_suite.success == True:
+            self.webhook = self.webhook.replace("{action}", self.action_if_pass)
+        else:
+            self.webhook = self.webhook.replace("{action}", self.action_if_fail)
+
         logger.debug("IFTTTNotificationAction.run")
 
         session = requests.Session()
 
         try:
-            response = session.post(url=webhook)
+            response = session.post(url=self.webhook)
         except requests.ConnectionError:
             logger.warning(
                 "Failed to connect to IFTTT webhook at {url} "
-                "after {max_retries} retries.".format(url=webhook, max_retries=10)
+                "after {max_retries} retries.".format(url=self.webhook, max_retries=10)
             )
         except Exception as e:
             logger.error(str(e))
@@ -119,7 +121,7 @@ class IFTTTNotificationAction(ValidationAction):
                 logger.warning(
                     "Request to IFTTT webhook at {url} "
                     "returned error {status_code}: {text}".format(
-                        url=webhook,
+                        url=self.webhook,
                         status_code=response.status_code,
                         text=response.text,
                     )
@@ -999,4 +1001,3 @@ class UpdateDataDocsAction(ValidationAction):
             data_docs_validation_results[sites["site_name"]] = sites["site_url"]
 
         return data_docs_validation_results
-
